d88df63c5ce4575e0b794b85f91845a05f8c0bba
bube <bubejanakievski@gmail.com>
2023-11-29 13:54:05 +0100

    new stuff

--- mypkg.go
+++ mypkg.go
@@ -8,4 +8,15 @@ import (
 func main() {
 	fmt.Println("mypkg")
 	inner.InnerFunc()
+	shouldPrint1 := false
+	if shouldPrint1 {
+		fmt.Println("1")
+	}
+	fmt.Println("2")
+	fmt.Println("3")
+
+	fmt.Println("start loop")
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
+	}
 }
